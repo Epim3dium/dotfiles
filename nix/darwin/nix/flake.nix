5d5b3197a84280b6336dcf7e42c365f9eef75074
Epim3dium <Epim3dium@protonmail.com>
2024-11-05 14:46:06 +0000

    added nix

---
+++ nix/darwin/nix/flake.nix
@@ -0,0 +1,106 @@
+{
+    description = "Epic Darwin system flake";
+
+    inputs = {
+        nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+        nix-darwin.url = "github:LnL7/nix-darwin";
+        nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+        nix-homebrew.url = "github:zhaofengli-wip/nix-homebrew";
+    };
+
+    outputs = inputs@{ self, nix-darwin, nixpkgs, nix-homebrew }:
+        let
+            configuration = { pkgs, config, ... }: {
+                # List packages installed in system profile. To search by name, run:
+                # $ nix-env -qaP | grep wget
+                nixpkgs.config.allowUnfree = true;
+                environment.systemPackages =
+                    [ 
+                        pkgs.neovim
+                        pkgs.mkalias
+                        pkgs.tmux
+
+                        # pkgs.hyprland
+                        pkgs.alt-tab-macos
+                        pkgs.wireshark
+                    ];
+                homebrew = {
+                    enable = true;
+                    brews = [
+                        "mas"
+                    ];
+                    casks = [
+                        "hammerspoon"
+                        "the-unarchiver"
+                        "intellij-idea-ce"
+                        "background-music"
+                        "rectangle"
+                    ];
+                    # onActivation.cleanup = "zap"
+                };
+                fonts.packages = [
+                    (pkgs.nerdfonts.override { fonts = [ "JetBrainsMono" ]; })
+                ];
+                system.activationScripts.applications.text = let
+                    env = pkgs.buildEnv {
+                        name = "system-applications";
+                        paths = config.environment.systemPackages;
+                        pathsToLink = "/Applications";
+                    };
+                in
+                    pkgs.lib.mkForce ''
+          # Set up applications.
+          echo "setting up /Applications..." >&2
+          rm -rf /Applications/Nix\ Apps
+          mkdir -p /Applications/Nix\ Apps
+          find ${env}/Applications -maxdepth 1 -type l -exec readlink '{}' + |
+          while read src; do
+            app_name=$(basename "$src")
+            echo "copying $src" >&2
+                        ${pkgs.mkalias}/bin/mkalias "$src" "/Applications/Nix Apps/$app_name"
+          done
+                    '';
+
+                # Auto upgrade nix package and the daemon service.
+                services.nix-daemon.enable = true;
+                # nix.package = pkgs.nix;
+
+                # Necessary for using flakes on this system.
+                nix.settings.experimental-features = "nix-command flakes";
+
+                # Enable alternative shell support in nix-darwin.
+                programs.zsh.enable = true;
+
+                # Set Git commit hash for darwin-version.
+                system.configurationRevision = self.rev or self.dirtyRev or null;
+
+                # Used for backwards compatibility, please read the changelog before changing.
+                # $ darwin-rebuild changelog
+                system.stateVersion = 5;
+
+                # The platform the configuration will be used on.
+                nixpkgs.hostPlatform = "aarch64-darwin";
+            };
+        in
+            {
+            # Build darwin flake using:
+            # $ darwin-rebuild build --flake .#simple
+            darwinConfigurations."Jans-MacBook-Air" = nix-darwin.lib.darwinSystem {
+                modules = [ 
+                    configuration 
+                    nix-homebrew.darwinModules.nix-homebrew
+                    {
+                      nix-homebrew = {
+                        enable = true;
+                        enableRosetta = true;
+                        user = "epi";
+                        autoMigrate = true;
+                      };
+                    }
+                ];
+            };
+
+            # Expose the package set, including overlays, for convenience.
+            darwinPackages = self.darwinConfigurations."Jans-MacBook-Air".pkgs;
+        };
+}

e01552afcc00abb83841509f4e054df625de8063
Epim3dium <Epim3dium@protonmail.com>
2025-03-18 13:52:21 +0100

    reverted color changes

--- modules/alacritty.nix
+++ modules/alacritty.nix
@@ -23,36 +23,33 @@
                 };
                 size = 12.0;
             };
-            colors = with config.colorScheme.colors; {
-                bright = {
-                    black = "0x${base00}";
-                    blue = "0x${base0D}";
-                    cyan = "0x${base0C}";
-                    green = "0x${base0B}";
-                    magenta = "0x${base0E}";
-                    red = "0x${base08}";
-                    white = "0x${base06}";
-                    yellow = "0x${base09}";
-                };
-                cursor = {
-                    cursor = "0x${base06}";
-                    text = "0x${base06}";
+            colors = {
+                primary = {
+                    background = "0x2E3440";
+                    foreground = "0xD8DEE9";
                 };
                 normal = {
-                    black = "0x${base00}";
-                    blue = "0x${base0D}";
-                    cyan = "0x${base0C}";
-                    green = "0x${base0B}";
-                    magenta = "0x${base0E}";
-                    red = "0x${base08}";
-                    white = "0x${base06}";
-                    yellow = "0x${base0A}";
+                    black   = "0x3B4252";
+                    red     = "0xBF616A";
+                    green   = "0xA3BE8C";
+                    yellow  = "0xEBCB8B";
+                    blue    = "0x81A1C1";
+                    magenta = "0xB48EAD";
+                    cyan    = "0x88C0D0";
+                    white   = "0xE5E9F0";
                 };
-                primary = {
-                    background = "0x${base00}";
-                    foreground = "0x${base06}";
+                bright = {
+                    black   = "0x4C566A";
+                    red     = "0xBF616A";
+                    green   = "0xA3BE8C";
+                    yellow  = "0xEBCB8B";
+                    blue    = "0x81A1C1";
+                    magenta = "0xB48EAD";
+                    cyan    = "0x8FBCBB";
+                    white   = "0xECEFF4";
                 };
             };
+
         };
     };
 }
